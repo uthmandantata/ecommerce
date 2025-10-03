d75d1973c28a0e62e05815050f8f111c013d013b
Uthman Dantata <uthmandantata8@gmail.com>
2025-10-03 05:55:38 +0100

    added models

---
+++ src/models/Order.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    products: [
+        {
+            productId: {
+                type: String
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            }
+        }
+    ],
+    amount: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: Object,
+        required: true
+    },
+    status: {
+        type: Number,
+        default: "pending"
+    },
+}, { timestamps: true });
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
---
+++ src/models/Product.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        default: ""
+    },
+    categories: {
+        type: Array
+    },
+    size: {
+        type: String
+    },
+    color: {
+        type: String
+    },
+    price: {
+        type: String,
+        required: true
+    },
+
+}, { timestamps: true });
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
